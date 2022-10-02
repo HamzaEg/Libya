1cb8253a5284eba6f5330d7fd77fad16c42ba106
Hamza <h.egnaidi@gmail.com>
2022-10-02 12:46:23 +0200

    add-migration and try to pusch 1st

---
+++ LibyaWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LibyaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibyaWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
